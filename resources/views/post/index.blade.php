a04c01680ee34da2bf9c6afd4cccab252b950638
Sahaj <sahajemast@gmail.com>
2020-04-22 16:32:05 +0530

    Version 4.0.2.4

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,7 +8,7 @@
                 <h5 class="text-dark">Latest Posts</h5>
                 @foreach($latestPosts as $latestPost)
                     <div class="mb-3 bg-white">
-                        <div class="card-header"><a class="text-dark" href="/{{ $latestPost->category->slug }}/{{ $latestPost->slug }}">{{ $latestPost->title }}</a></div>
+                        <div class="card-header"><a class="text-dark" href="{{ route('posts.show', [$latestPost->category->slug, $latestPost->slug]) }}">{{ $latestPost->title }}</a></div>
 
                         <div class="card-body">
                             {{ $latestPost->excerpt }}
